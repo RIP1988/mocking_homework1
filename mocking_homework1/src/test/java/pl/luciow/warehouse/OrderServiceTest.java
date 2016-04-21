d6741b86be565368c830ea5ca591cdd51ff730f4
RIP1988 <BPOLANSK@CE14798.corp.capgemini.com>
2016-04-21 07:49:50 +0200

    Wrzutka na Githuba

---
+++ mocking_homework1/src/test/java/pl/luciow/warehouse/OrderServiceTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.luciow.warehouse;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Mariusz
+ */
+public class OrderServiceTest {
+
+    private OrderService orderService;
+
+    @Test
+    public void fillOrderSuccesTest() {
+    }
+
+    @Test
+    public void fillOrderThrowTest() {
+    }
+
+    @Test
+    public void cancelOrderTest() {
+    }
+
+    @Test
+    public void processPaymentThrowTest() {
+    }
+
+    @Test
+    public void processPaymentSuccessTest() {
+    }
+
+}
