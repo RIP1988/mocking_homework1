baee9ce920315ffdbb0e489afa4d485ea637e3f9
RIP1988 <BPOLANSK@CE14798.corp.capgemini.com>
2016-04-26 14:51:03 +0200

    koniec

--- mocking_homework1/src/test/java/pl/luciow/warehouse/impl/PaymentServiceImplTest.java
+++ mocking_homework1/src/test/java/pl/luciow/warehouse/impl/PaymentServiceImplTest.java
@@ -5,12 +5,19 @@
  */
 package pl.luciow.warehouse.impl;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
 import pl.luciow.warehouse.PaymentProcessor;
+import pl.luciow.warehouse.model.Payment;
 import pl.luciow.warehouse.util.PaymentValidator;
 import pl.luciow.warehouse.util.ValidatorException;
 
@@ -25,13 +32,29 @@ public class PaymentServiceImplTest {
     private PaymentServiceImpl paymentServiceImpl = new PaymentServiceImpl();
 
     @Mock
-    private PaymentProcessor paymentProcessor;
+    private PaymentProcessor paymentProcessorMock;
 
     @Mock
-    private PaymentValidator paymentValidator;
+    private PaymentValidator paymentValidatorMock;
 
-    @Test(expected = ValidatorException.class)
-    public void processPaymentTest() throws Exception {
-
+    @Test
+    public void processPaymentTest() throws Exception {	    	
+    			Mockito.doAnswer(new Answer() {
+    			      public Object answer(InvocationOnMock invocation) {
+    			          Object[] args = invocation.getArguments();
+    			          @SuppressWarnings("unchecked")
+    					List<String> errors = (List<String>) args[1];
+    						errors.add("error");
+    						return null;
+    			      }})
+    			  .when(paymentValidatorMock).validate(Mockito.any(Payment.class), Mockito.anyListOf(String.class));
+    			try {
+    				paymentServiceImpl.processPayment(new Payment());
+    				Mockito.verify(paymentProcessorMock, Mockito.times(1)).processPayment(Mockito.any(Payment.class));
+    			} catch (ValidatorException e) {
+    				Mockito.verify(paymentProcessorMock, Mockito.times(0)).processPayment(Mockito.any(Payment.class));
+    			}
+    			
+    		}
     }
-}
+
